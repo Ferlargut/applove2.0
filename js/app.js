1d87b6c15694755dbd874d2e74fd95c715e66373
Ferlargut <ferlargut@gmail.com>
2017-05-03 15:37:58 -0500

    funcionalidad

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+var imagenes = document.getElementsByClassName("imgwork");
+var arregloimagenes = Array.from(imagenes);
+var modal1 = document.getElementById("modalimg");
+var modalimg = document.getElementById("imagenmodal");
+var close = document.getElementById("close");
+
+
+arregloimagenes.forEach(function (img){
+    img.addEventListener("click",hola);    
+function hola(item){
+    modal1.style.display = "block";
+    modalimg.src = img.src;    
+}
+}); 
+close.addEventListener("click",cerrar);
+
+function cerrar(){
+    modal1.style.display = "none";
+}
+
